fe6e34623ba46d95690c97f6e92dbb59f84ff784
BrayanJL <brayan.j.m.lucero@gmail.com>
2024-05-16 11:27:28 -0300

    Tipo de dato de nota cambiado a Double

--- registrodealumnos.sql
+++ registrodealumnos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2024 a las 16:30:02
+-- Tiempo de generación: 16-05-2024 a las 16:25:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,7 +44,7 @@ CREATE TABLE `alumno` (
 
 CREATE TABLE `inscripcion` (
   `idInscripto` int(11) NOT NULL,
-  `nota` int(11) NOT NULL,
+  `nota` double NOT NULL,
   `idAlumno` int(11) NOT NULL,
   `idMateria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
